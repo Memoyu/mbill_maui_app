9e0cd1661a0de70b532868dc898a8cba95ffe7e7
Memoyu <mmy6076@outlook.com>
2021-04-09 21:16:47 +0800

    fix：Bottom Tabbar

--- mbill.xamarin.app/mbill.xamarin.app.Android/MainActivity.cs
+++ mbill.xamarin.app/mbill.xamarin.app.Android/MainActivity.cs
@@ -12,6 +12,8 @@ namespace mbill.xamarin.app.Droid
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
             base.OnCreate(savedInstanceState);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
--- mbill.xamarin.app/mbill.xamarin.app/Views/Index/HomePage.xaml.cs
+++ mbill.xamarin.app/mbill.xamarin.app/Views/Index/HomePage.xaml.cs
@@ -5,16 +5,20 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
 using Xamarin.Forms.Xaml;
 
 namespace mbill.xamarin.app.Views.Index
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class HomePage : TabbedPage
+    public partial class HomePage : Xamarin.Forms.TabbedPage
     {
         public HomePage()
         {
             InitializeComponent();
+            // vm = BindingContext as MusicHomePageViewModel;
+            this.On<Xamarin.Forms.PlatformConfiguration.Android>().DisableSwipePaging();
+            this.On<Xamarin.Forms.PlatformConfiguration.Android>().SetToolbarPlacement(ToolbarPlacement.Bottom);
         }
     }
 }
